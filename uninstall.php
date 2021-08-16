7f51d0184a422fa762895f70c32072c639f13cdb
Lee Stratman <lee.stratman@protonmail.com>
2021-08-16 15:16:20 -0500

    Add uninstall file.

---
+++ uninstall.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Sample Plugin Uninstall
+ *
+ * A plugin is considered uninstalled if a user has deactivated the plugin,
+ * and then clicks the delete link within the WordPress Admin.
+ *
+ * @see https://developer.wordpress.org/plugins/plugin-basics/uninstall-methods/
+ * @package PluginPackage
+ */
+
+// Add clean-up code here.
